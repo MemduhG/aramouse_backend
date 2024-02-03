b33a0c39c4cd1337cd9a63df18c4db4b68c67c00
Memduh Gokirmak <memduhg@gmail.com>
2024-02-03 02:01:38 +0100

    web interface

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+import json
+
+from flask import Flask, request, jsonify, render_template
 import pyaramorph
 
 analyzer = pyaramorph.Analyzer()
@@ -11,18 +13,52 @@ def analyze(s):
 
 app = Flask(__name__)
 
+with open("static/translit.json") as infile:
+    translit_dict = json.load(infile)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def translit(text):
+    transliterated = ""
+    for char in text:
+        if char in translit_dict:
+            transliterated += translit_dict[char]
+        else:
+            transliterated += char
+
+    return transliterated
+
+
+def analyze_text(inp):
+    text = translit(inp)
+    print(text)
+    analysis = analyze(text)
+    print(analysis)
+    return_list = []
+    for item in analysis:
+        item_list = []
+        for line in item:
+            item_list.append(line)
+
+        if len(item_list) > 0:
+            return_list.append(item_list)
+    return jsonify({"analyzed_text": return_list})
+
 
 @app.route('/post_endpoint', methods=['POST'])
 def post_example():
     data = request.get_data().decode("utf-8")
-    analysis = analyze(data)
-    return_text = ""
-    print(data)
-    for item in analysis:
-        for line in item:
-            return_text += line
-        return_text += "\n"
-    return jsonify(return_text.strip())
+    return analyze_text(data)
+
+
+@app.route('/html_endpoint', methods=['POST'])
+def post_html():
+    data = json.loads(request.get_data().decode("utf-8"))
+    return analyze_text(data["text"])
 
 
 if __name__ == '__main__':
